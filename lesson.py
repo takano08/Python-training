d446188f1de5caabf1b11b5d310a86ad320cd3e5
takano <ri1saxxminny83xx@gmail.com>
2021-02-06 21:49:35 +0900

    文字列表示

--- lesson.py
+++ lesson.py
@@ -1,6 +1,26 @@
-import math
+print('hello')
+print("hello")
+print("I don't know")
+print('I don\'t know')
+print('say "I don\'t know"')
 
-result = math.sqrt(25)
-print(result)
+print('hello. \nHow are you?')
+print(r'C:\name\name')
 
-print(help(math))
+print("###########")
+print("""\
+line1
+line2
+line3\
+""")
+print("##########")
+
+print('hi.' * 3 + 'Mike.')
+
+print('Py' + 'thon')
+prefix= 'Py'
+print(prefix +'thon')
+s= ('aaaaaaaaaaaaaaaaaaaaa'
+    'bbbbbbbbbbbbbbbbbbbbb')
+
+print(s)
