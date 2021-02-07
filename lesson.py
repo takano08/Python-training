05cb029aa34d751861f5fa57d92c9cee0ac02360
takano <ri1saxxminny83xx@gmail.com>
2021-02-07 22:16:31 +0900

    文字列のインデックスとスライス1

--- lesson.py
+++ lesson.py
@@ -24,3 +24,11 @@ s= ('aaaaaaaaaaaaaaaaaaaaa'
     'bbbbbbbbbbbbbbbbbbbbb')
 
 print(s)
+
+word ='python'
+print(word[0])
+print(word[1])
+print(word[-1])
+print(word[0:2])
+print(word[:2])
+print(word[2:])
