27b3f0f4dde563c848d05720890dac221b839c69
takano <ri1saxxminny83xx@gmail.com>
2021-02-08 11:19:43 +0900

    文字のメソッド1

--- lesson.py
+++ lesson.py
@@ -1,39 +1,11 @@
-print('hello')
-print("hello")
-print("I don't know")
-print('I don\'t know')
-print('say "I don\'t know"')
-
-print('hello. \nHow are you?')
-print(r'C:\name\name')
-
-print("###########")
-print("""\
-line1
-line2
-line3\
-""")
-print("##########")
-
-print('hi.' * 3 + 'Mike.')
-
-print('Py' + 'thon')
-prefix= 'Py'
-print(prefix +'thon')
-s= ('aaaaaaaaaaaaaaaaaaaaa'
-    'bbbbbbbbbbbbbbbbbbbbb')
-
+s = 'My name is Mike. Hi Mike.'
 print(s)
+is_start =s.startswith('My')
+print(is_start)
+is_start =s.startswith('X')
+print(is_start)
 
-word ='python'
-print(word[0])
-print(word[1])
-print(word[-1])
-print(word[0:2])
-print(word[:2])
-print(word[2:])
-word ='j' + word[1:]
-print(word)
-print(word[:])
-n = len(word)
-print(n)
+print("#################")
+
+print(s.find('Mike'))
+print(s.rfind('Mike'))
