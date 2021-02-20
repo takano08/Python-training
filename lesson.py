1cf25406ab460f9df7b2ba4578e5670e0179d3ff
takano <ri1saxxminny83xx@gmail.com>
2021-02-20 21:50:49 +0900

    タプル型のアンパッキング1

--- lesson.py
+++ lesson.py
@@ -1,52 +1,8 @@
-r = [1,2,3,4,5,1,2,3]
-print(r.index(3,3))
+num_tuple =(10,20)
+print(num_tuple)
 
-print(r.count(3))
-if 5 in r:
-    print('exist')
+x,y = num_tuple
+print(x,y)
 
-if 100 in r:
-    print('exist')
-
-r.sort()
-print(r)
-r.sort(reverse=True)
-print(r)
-r.reverse()
-print(r)
-
-s = 'My name is Mike'
-to_split = s.split(' ')
-print(to_split)
-
-x = ' '.join(to_split)
-print(x)
-
-i =[1,2,3,4,5]
-j = i
-j[0] = 100
-print('j=',j)
-print('i =',i)
-
-x = [1,2,3,4,5,]
-# y = x.copy()
-y = x[:]
-y[0] = 100
-print('y=',y)
-print('x=',x)
-
-X = 20
-Y = X
-Y = 5
-print(id(X))
-print(id(Y))
-print(Y)
-print(X)
-
-X = ['a','b']
-Y=X
-Y[0] = 'p'
-print(id(X))
-print(id(Y))
-print(Y)
-print(X)
+x,y = 10,20
+print(x,y)
