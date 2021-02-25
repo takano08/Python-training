f2dfd1a2014b5d96b0f78a766d3097e709c486db
takano <ri1saxxminny83xx@gmail.com>
2021-02-25 10:33:03 +0900

    辞書の使い所

--- lesson.py
+++ lesson.py
@@ -1,11 +1,7 @@
-x = {'a':1}
-y = x
-y['a'] = 1000
-print(x)
-print(y)
+fruits = {
+    'apple': 100,
+    'banana': 200,
+    'orange': 300,
+}
 
-x = {'a' :1}
-y = x.copy()
-y['a'] = 1000
-print(x)
-print(y)
+print(fruits['apple'])
