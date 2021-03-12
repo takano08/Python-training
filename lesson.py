4c8c567b8d541e4ace0c3c4bb95c08530b86a740
takano <ri1saxxminny83xx@gmail.com>
2021-03-12 10:04:57 +0900

    比較演算子と論理演算子1

--- lesson.py
+++ lesson.py
@@ -1,18 +1,7 @@
-x = 10
+a = 1
+b = 1
 
-if x < 0:
-    print('negative')
-elif x == 0:
-    print('zero')
-elif x == 10:
-    print('10')
-else:
-    print('positive')
+if a == b:
+    print('equal')
 
-a = 5
-b = 10
-
-if a > 0:
-    print('a is positive')
-    if b > 0:
-        print('b is positive')
+print(a != b)
