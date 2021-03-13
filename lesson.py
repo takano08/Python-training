db9ae95f74e7ac04f9781b0e71e8dfe55dd66867
takano <ri1saxxminny83xx@gmail.com>
2021-03-13 23:33:27 +0900

    比較演算子と論理演算子2

--- lesson.py
+++ lesson.py
@@ -1,7 +1,22 @@
 a = 1
-b = 1
+b = 2
 
-if a == b:
-    print('equal')
+a == b
 
-print(a != b)
+a != b
+
+a<b
+
+print(a<=b)
+
+if a>0:
+    if b>0:
+        print('a and b are positive')
+
+if a>0 and b>0:
+    print('a and b are positive')
+
+a =-1
+b=1
+if a>0 or b>0:
+    print('a or b are positive')
