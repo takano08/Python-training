1a1e4af791726617c70124ca2444731199b4f2b8
takano <ri1saxxminny83xx@gmail.com>
2021-03-14 23:08:48 +0900

    inとnotの使い所

--- lesson.py
+++ lesson.py
@@ -1,22 +1,7 @@
-a = 1
-b = 2
+y = [1,2,3]
+x = 1
+if x in y:
+    print('in')
 
-a == b
-
-a != b
-
-a<b
-
-print(a<=b)
-
-if a>0:
-    if b>0:
-        print('a and b are positive')
-
-if a>0 and b>0:
-    print('a and b are positive')
-
-a =-1
-b=1
-if a>0 or b>0:
-    print('a or b are positive')
+if 100 not in y:
+    print('not in')
