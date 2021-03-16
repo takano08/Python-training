9d0f8ca2a1865cdddcb03ad3bf7b8ad08346e671
takano <ri1saxxminny83xx@gmail.com>
2021-03-16 10:54:21 +0900

    値が入っていない判定をするテクニック

--- lesson.py
+++ lesson.py
@@ -1,16 +1,6 @@
-y = [1,2,3]
-x = 1
-if x in y:
-    print('in')
+is_ok = ''
 
-if 100 not in y:
-    print('not in')
-
-a = 1
-b = 2
-
-if not a == b:
-    print('Not equal')
-
-if not a != b:
-    print('Not equal')
+if is_ok:
+    print('OK')
+else:
+    print('NO')
