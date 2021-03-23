2a0563a49450ab2ffcf1063143d0ba80280743f7
takano <ri1saxxminny83xx@gmail.com>
2021-03-23 22:14:03 +0900

    for文

--- lesson.py
+++ lesson.py
@@ -1,5 +1,6 @@
-while True:
-    word = input('Enter')
-    if word == 'ok':
-        break
-    print('next')
+some_list = [1,2,3,4,5]
+
+i =0
+while i < len(some_list):
+    print(some_list[i])
+    i += 1
