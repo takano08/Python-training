34a3d9f89e6456ee0be294c92b2787aa5632342c
takano <ri1saxxminny83xx@gmail.com>
2021-03-31 09:55:38 +0900

    関数の引数と戻り値の宣言

--- lesson.py
+++ lesson.py
@@ -1,15 +1,5 @@
-def say_something():
-    s = 'hi'
-    return s
-result = say_something()
-print(result)
+def add_num(a: int, b: int) -> int:
+    return a + b
 
-def what_is_this(color):
-    if color == 'red':
-        return 'tomato'
-    elif color == 'green':
-        return 'green pepper'
-    else:
-        return "I don't know"
-result = what_is_this('red')
-print(result)
+r = add_num(10,20)
+print(r)
