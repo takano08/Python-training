32e6ce5a98161ca8e40d531368d537df468f5476
takano <ri1saxxminny83xx@gmail.com>
2021-04-02 09:35:36 +0900

    デフォルト引数で気をつけること

--- lesson.py
+++ lesson.py
@@ -1,6 +1,10 @@
-def menu(entree='beef',drink='wine',dessert='ice'):
-    print(entree)
-    print(drink)
-    print(dessert)
+def test_func(x, l=[]):
+    l.append(x)
+    return l
 
-menu()
+# y = [1,2,3]
+# r = test_func(100,y)
+# print(r)
+
+r = test_func(100)
+print(r)
